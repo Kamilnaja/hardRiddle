c34437e12073271693aa59bcd72691e4dc637530
Kamil Naja <kamilnaja@gmail.com>
2017-03-28 21:22:25 +0200

    added input

--- controller.js
+++ controller.js
@@ -2,21 +2,27 @@
  * Created by Kamil on 2017-03-27.
  */
 var app = angular.module('mainApp', ['ngRoute']);
-
 app.config(['$locationProvider', function ($locationProvider) {
     $locationProvider.hashPrefix('');
 }]);
+app.controller('MyController', function ($scope) {
+
+});
 
 app.config(function($routeProvider) {
     $routeProvider
         .when('/', {
-            template: 'Welcome user on default'
+            template:
+                `<h1>Welcome user on default</h1>
+                <p>Lets fight with first riddle</p>
+                <p>Gdy będziesz gotowy, wpisz odpowiedź w inpu</p>
+`
         })
         .when('/another', {
-            template: 'Welcome user again'
+            template: `Welcome user again`
         })
         .when('/thirdriddle', {
-            template: 'Let fight with third riddle'
+            template: `Let fight with third riddle`
         })
         .otherwise({
             redirectTo: '/'
